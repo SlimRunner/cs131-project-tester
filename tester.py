ac3366e400e4920c9550754cab1defe5d7a6f858
SlimRunner <dv.polygon@gmail.com>
2024-10-23 23:09:30 -0700

    Make tester more general

--- tester.py
+++ tester.py
@@ -6,14 +6,13 @@ import difflib
 from unittest.mock import patch
 
 
-class Tester:
+class TesterBase:
     CODE = "code"
     USER_INPUT = "stdin"
     PROG_OUTPUT = "stdout"
     ERROR_OUTPUT = "error"
 
     def __init__(self, filepath: str, callback, *args):
-        self.collecting = True
         self.callback = callback
         self.arguments = args
         self.sections = self.blank_section()
@@ -34,46 +33,11 @@ class Tester:
 
                 state = self.advance_fsm(state, line)
 
-        # print(self.sections)
-        pass
-
     def blank_section(self):
         return {"code": [], "stdin": [], "stdout": [], "error": []}
 
     def run_section(self):
-        program_source = "\n".join(self.sections[Tester.CODE])
-        user_input = self.sections[Tester.USER_INPUT]
-
-        with patch("builtins.input", side_effect=user_input):
-            stdout_buff = io.StringIO()
-            stderr_buff = io.StringIO()
-            sys.stdout = stdout_buff
-            sys.stderr = stderr_buff
-            try:
-                self.callback(program_source)
-
-            except Exception as e:
-                error_message = e.args[0].split(":", 1)[0]
-                print(error_message, file=sys.stderr)
-            sys.stdout = sys.__stdout__  # Restore original stdout
-
-        print()
-        self.match_buffer(stdout_buff.getvalue(), Tester.PROG_OUTPUT, "output")
-        self.match_buffer(stderr_buff.getvalue(), Tester.ERROR_OUTPUT, "error")
-        print()
-
-    def match_buffer(self, recieved: str, tag: str, msg: str):
-        if recieved.endswith("\n"):
-            recieved = recieved[:-1]
-
-        expected = "\n".join(self.sections[tag])
-        if recieved == expected:
-            print(f"{msg}: pass ✔")
-        else:
-            TAB = "  "
-            print(f"{msg}: FAIL ❌")
-            diff = difflib.ndiff(recieved.splitlines(), expected.splitlines())
-            print('\n'.join([TAB + l for l in diff if l.startswith("-") or l.startswith("+")]))
+        pass
 
     def update_sections(self, state, line):
         if line == "```":
@@ -81,7 +45,6 @@ class Tester:
 
         match state:
             case "code" | "stdin" | "stdout" | "error":
-                self.collecting = True
                 if state not in self.sections:
                     self.sections[state] = [line]
                 else:
@@ -125,3 +88,44 @@ class Tester:
                 self.run_section()
 
         return state
+
+
+class Tester(TesterBase):
+    def run_section(self):
+        program_source = "\n".join(self.sections[TesterBase.CODE])
+        user_input = self.sections[TesterBase.USER_INPUT]
+
+        with patch("builtins.input", side_effect=user_input):
+            stdout_buff = io.StringIO()
+            stderr_buff = io.StringIO()
+            sys.stdout = stdout_buff
+            sys.stderr = stderr_buff
+            try:
+                self.callback(program_source)
+
+            except Exception as e:
+                error_message = e.args[0].split(":", 1)[0]
+                print(error_message, file=sys.stderr)
+            sys.stdout = sys.__stdout__  # Restore original stdout
+
+        print()
+        self.match_buffer(stdout_buff.getvalue(), TesterBase.PROG_OUTPUT, "output")
+        self.match_buffer(stderr_buff.getvalue(), TesterBase.ERROR_OUTPUT, "error")
+        print()
+
+    def match_buffer(self, recieved: str, tag: str, msg: str):
+        if recieved.endswith("\n"):
+            recieved = recieved[:-1]
+
+        expected = "\n".join(self.sections[tag])
+        if recieved == expected:
+            print(f"{msg}: pass ✔")
+        else:
+            TAB = "  "
+            print(f"{msg}: FAIL ❌")
+            diff = difflib.ndiff(recieved.splitlines(), expected.splitlines())
+            print(
+                "\n".join(
+                    [TAB + l for l in diff if l.startswith("-") or l.startswith("+")]
+                )
+            )
